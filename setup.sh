1261c80152706f7c33bd9f97c5fe9c9d8a0d189c
Fabian Thoma <f@bianthoma.me>
2019-09-15 14:32:39 +0200

    Add Emacs .desktop file with window manager fixes

--- setup.sh
+++ setup.sh
@@ -6,3 +6,5 @@ git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 
 rm ~/.bashrc
 ln -s ~/projects/dotfiles/.bashrc ~/.bashrc
+
+cp ~/projects/dotfiles/emacs26.desktop ~/.local/share/applications/emacs26.desktop
